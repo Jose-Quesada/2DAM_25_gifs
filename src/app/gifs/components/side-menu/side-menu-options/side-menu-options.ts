10ce68936c0f29025f213e330532616a3dfd0858
Jose-Quesada <jose.quesada@fernando3martos.com>
2026-01-09 12:44:31 +0100

    9 enero

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink],
+  templateUrl: './side-menu-options.html',
+})
+export class SideMenuOptions {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
+
+
--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenu } from "../../components/side-menu/side-menu";
+
 
 @Component({
   selector: 'gifs-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './dashboard-page.html',
 })
 export default class DashboardPage { }
